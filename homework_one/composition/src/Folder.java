96be33356d55ef972d7c7a97a01a837b2308e7b7
Artin Ohanessian <artinohanessian@gmail.com>
2024-02-12 04:45:18 -0800

    composition

--- homework_one/composition/src/Folder.java
+++ homework_one/composition/src/Folder.java
@@ -1,6 +1,27 @@
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class Folder {
+
+    private class PathData {
+        private String nextFolder;
+        private String remainingPath;
+
+        PathData(String nextFolder, String remainingPath) {
+            this.nextFolder = nextFolder;
+            this.remainingPath = remainingPath;
+        }
+
+        public String getNextFolder() {
+            return nextFolder;
+        }
+
+        public String getRemainingPath() {
+            return remainingPath;
+        }
+
+    }
+
     private String name;
     private ArrayList<File> files;
     private ArrayList<Folder> subFolders;
@@ -35,30 +56,112 @@ public class Folder {
         subFolders.add(new Folder(name));
     }
 
-    public boolean removeSubFolder(String name) {
+    private boolean endOfPath(String path) {
+        return path == null || path.isEmpty() || path.equals("/");
+    }
+
+    private Optional<PathData> getPathData(String path) {
+        if (endOfPath(path)) {
+            return Optional.empty();
+        }
+
+        String[] pathParts = path.split("/", 2);
+        return Optional.of(new PathData(pathParts[0], pathParts.length > 1 ? pathParts[1] : ""));
+    }
+
+    public boolean addFileToPath(String path, String name) {
+        Optional<PathData> pathData = getPathData(path);
+        if (pathData.isEmpty()) {
+            addFile(name);
+            return true;
+        }
+
         for (Folder folder : subFolders) {
-            if (folder.getName().equals(name)) {
-                subFolders.remove(folder);
-                return true;
+            if (folder.getName().equals(pathData.get().getNextFolder())) {
+                return folder.addFileToPath(pathData.get().getRemainingPath(), name);
             }
         }
+
+        System.out.println("Path not found " + path);
         return false;
     }
 
-    public void print() {
-        StringBuilder result = new StringBuilder("Folder: " + name + "\n");
-        result.append("Files:\n");
-        for (File file : files) {
-            result.append(file.getName()).append("\n");
-        }
-        result.append("Subfolders:\n");
-        for (Folder folder : subFolders) {
-            result.append(folder.getName()).append("\n");
-            for (File file : folder.getFiles()) {
-                result.append("\t").append(file.getName()).append("\n");
+    public boolean removeFileFromPath(String path, String fileName) {
+        Optional<PathData> pathData = getPathData(path);
+        if (pathData.isPresent()) {
+            for (Folder folder : subFolders) {
+                if (folder.getName().equals(pathData.get().getNextFolder())) {
+                    return folder.removeFileFromPath(pathData.get().getRemainingPath(), fileName);
+                }
             }
         }
-        System.out.println(result);
+
+        System.out.println("Path not found: " + path);
+        return false;
+    }
+
+    public boolean removeSubFolderFromPath(String path, String folderName) {
+        Optional<PathData> pathData = getPathData(path);
+        if (pathData.isPresent()) {
+            for (Folder folder : subFolders) {
+                if (folder.getName().equals(pathData.get().getNextFolder())) {
+                    if (pathData.get().getRemainingPath().isEmpty()) {
+                        return folder.removeSubFolder(folderName);
+                    } else {
+                        return folder.removeSubFolderFromPath(pathData.get().getRemainingPath(), folderName);
+                    }
+                }
+            }
+        }
+
+        System.out.println("Path not found: " + path);
+        return false;
+    }
+
+
+    public boolean addFolderToPath(String path) {
+        Optional<PathData> pathData = getPathData(path);
+        if (pathData.isEmpty()) {
+            System.out.println("Invalid folder name " + path);
+            return false;
+        }
+
+        for (Folder folder : subFolders) {
+            if (folder.getName().equals(pathData.get().getNextFolder())) {
+                return folder.addFolderToPath(pathData.get().getRemainingPath());
+            }
+        }
+
+        addSubFolder(pathData.get().getNextFolder());
+        return true;
+    }
+
+    public boolean removeSubFolder(String name) {
+        return subFolders.removeIf(folder -> folder.getName().equals(name));
+    }
+
+    public boolean removeFile(String name) {
+        return files.removeIf(file -> file.getName().equals(name));
+    }
+
+    public void print() {
+        printRecursive(0);
+    }
+
+    private void printRecursive(int level) {
+        String indent = "\t".repeat(level);
+        System.out.println(indent + "Folder: " + name);
+
+        if (!files.isEmpty()) {
+            for (File file : files) {
+                System.out.println(indent + "\tFiles: " + file.getName());
+            }
+        }
+        if (!subFolders.isEmpty()) {
+            for (Folder subFolder : subFolders) {
+                subFolder.printRecursive(level + 1);
+            }
+        }
     }
 
 }
--- homework_one/composition/src/Main.java
+++ homework_one/composition/src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
+        Folder folder = new Folder("demo1");
+
+
+        folder.addFolderToPath("Source Files");
+        folder.addFolderToPath("Include Path");
+        folder.addFolderToPath("Remote Files");
+        folder.addFolderToPath("Source Files/.phalcon");
+        folder.addFolderToPath("Source Files/app/config");
+        folder.addFolderToPath("Source Files/app/controllers");
+        folder.addFolderToPath("Source Files/app/library");
+        folder.addFolderToPath("Source Files/app/migrations");
+        folder.addFolderToPath("Source Files/app/models");
+        folder.addFolderToPath("Source Files/app/views");
+        folder.addFolderToPath("Source Files/cache");
+        folder.addFolderToPath("Source Files/public");
+        folder.addFileToPath("Source Files/public/", ".htaccess");
+        folder.addFileToPath("Source Files/public/", ".htrouter.php");
+        folder.addFileToPath("Source Files/public/", "index.html");
+
+        folder.print();
+        System.out.println("\n\n--------------------\n\n");
+
+        folder.removeSubFolderFromPath("Source Files", "app");
+
+        folder.print();
+
+        System.out.println("\n\n--------------------\n\n");
+
+        folder.removeSubFolderFromPath("Source Files", "public");
+
+        folder.print();
 
     }
 }
